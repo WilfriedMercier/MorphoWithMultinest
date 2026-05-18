1d55be6312ed9afaa556629d9c845a8055effd02
Wilfried <wifried.mercier.pro@pm.me>
2026-05-18 16:47:46 +0200

    added an example

---
+++ examples/JADES-GS-z14-0/run_fit.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+r'''
+.. codeauthor:: Wilfried Mercier - LAM <wilfried.mercier@lam.fr>
+
+Run the fit for the galaxy JADES-GS-z14-0.
+'''
+
+from MorphoWithMultinest import run
+
+if __name__ == '__main__': run('configuration.yaml')
